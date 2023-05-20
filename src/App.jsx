bed2e2fc933e7e822a2a5c2dc4877103fc681b9e
Mishkile <michaelshami263@gmail.com>
2023-05-20 09:27:00 +0300

    Final V1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import GetLengthPage from './Pages/GetLengthPage'
+
+function App() {
+
+  return (
+    <>
+     <GetLengthPage />
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/GetLengthPage.jsx
@@ -0,0 +1,26 @@
+import axios from "axios"
+import { useState } from "react"
+function GetLengthPage() {
+    const [folderID, setFolderID] = useState("")
+    const [length, setLength] = useState("")
+    const getData = async () => {
+        const { data } = await axios.post("https://rich-ruby-bream-ring.cyclic.app/", { folderID })
+        setLength(data.time)
+    }
+    return (
+        <div>
+
+
+            <input onChange={(e) => setFolderID(e.target.value)} placeholder="Enter Folder ID" />
+            <br />
+            <button onClick={getData}>Get Length!</button> <br />
+
+            Total Length is: <br />
+            Hours: {length[0]} <br />
+            Minutes: {length[1]} <br />
+            Seconds: {length[2]} <br />
+        </div>
+    )
+}
+
+export default GetLengthPage
